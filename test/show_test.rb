714e15d29e2c267e59a10d5f3ea92d575441034e
Jenny Branham <branhamjenny@gmail.com>
2020-12-15 10:14:26 -0600

    Add total salary test and method

--- test/show_test.rb
+++ test/show_test.rb
@@ -22,4 +22,13 @@ class ShowTest < MiniTest::Test
     assert_equal "Glen Larson", knight_rider.creator
     assert_equal [michael_knight, kitt], knight_rider.characters
   end
+
+  def test_total_salary
+    character_hash = {name: "KITT", actor: "William Daniels", salary: 1_000_000}
+    kitt = Character.new(character_hash)
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
 end
